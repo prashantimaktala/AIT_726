0776d40b8901cc1f6d9172933d58d8c6c323112f
Yasas Senarath <ysenarath.93@gmail.com>
2020-02-16 13:50:47 -0500

    Add pre-processing functions.

--- HW1/train.py
+++ HW1/train.py
@@ -1,5 +1,14 @@
 import os
+import re
+import string
+
 import pandas as pd
+from nltk import word_tokenize
+from nltk.stem.porter import PorterStemmer
+
+stemmer = PorterStemmer()
+
+emoticons_pattern = r'(:\)|:-\)|:\(|:-\(|;\);-\)|:-O|8-|:P|:D|:\||:S|:\$|:@|8o\||\+o\(|\(H\)|\(C\)|\(\?\))'
 
 
 def read_files(path):
@@ -14,6 +23,51 @@ def read_files(path):
     return df
 
 
+def tokenize(x, stem=False):
+    x = re.sub(r'(?:<[^>]+>)', '', x)
+    x = re.sub('([A-Z][a-z]+)', lambda t: t.group(0).lower(), x)
+    emoticon_tokens = re.split(emoticons_pattern, x)
+    tokens = []
+    for t in emoticon_tokens:
+        if re.match(emoticons_pattern, t):
+            tokens.append(t)
+        else:
+            tokens += word_tokenize(x)
+    if stem:
+        tokens = [stemmer.stem(t) for t in tokens]
+    return tokens
+
+
+def vocabulary(tokenized_tweets):
+    vocab = set()
+    for tokens in tokenized_tweets:
+        vocab.update(tokens)
+    return list(vocab)
+
+
+def bag_of_words(tokenized_tweet, vocab, binary=True):
+    if binary:
+        return [1 if v in tokenized_tweet else 0 for v in vocab]
+    else:
+        return [tokenized_tweet.count(v) if v in tokenized_tweet else 0 for v in vocab]
+
+
+def preprocess(df):
+    df['tokens'] = df.tweet.apply(tokenize)
+    df['stemmed_tokens'] = df.tweet.apply(lambda x: tokenize(x, True))
+    vocab = vocabulary(df.tokens)
+    stemmed_vocab = vocabulary(df.stemmed_tokens)
+    # Binary
+    bow_bin = df.tokens.apply(lambda x: bag_of_words(x, vocab))
+    bow_stemmed_bin = df.tokens.apply(lambda x: bag_of_words(x, vocab))
+    # Frequency
+    bow_freq = df.tokens.apply(lambda x: bag_of_words(x, vocab, False))
+    bow_stemmed_freq = df.tokens.apply(lambda x: bag_of_words(x, vocab, False))
+    print(vocab)
+    return df
+
+
 if __name__ == '__main__':
     df_train = read_files('./data/tweet/train')
-    print(df_train.head())
+    df_train = preprocess(df_train)
+    print(df_train.columns)

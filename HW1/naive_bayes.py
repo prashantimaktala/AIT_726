06982d24a2c8d35749a72c3d2f047fc1bcfbd643
prashantimaktala <prashantimaktala@gmail.com>
2020-02-20 21:17:48 -0500

    Added flow and comments

--- HW1/naive_bayes.py
+++ HW1/naive_bayes.py
@@ -1,20 +1,38 @@
 """
 AIT726 HW 1 Due 2/20/2020
 Sentiment classification using Naive Bayes and Logistic Regression on a dataset of 4181 training and 4182 testing tweets.
-Authors: Yasas, Prashanti , Ashwini
+Authors: Yasas, Prashanti, Ashwini
 Command to run the file: naive_bayes.py
+
+Flow:
+i. main
+ii. run  default parameters: stem = false, binary = true
+    1.Train the model
+        a. Read the dataset
+        b. Perform preprocessing
+            - Build vocab
+        c. Train the model
+            - calculate prior and likelihood
+    2. Test the model
+        a. Read the dataset
+        b. Perform preprocessing
+            - tokenize
+        c. Predict
+        d. Evaluate the model
+            - Save confusion matrix and accuracy to log file
+
 """
 
 import os
 import re
-
+import logging
 import numpy as np
 import pandas as pd
 from nltk import word_tokenize
 from nltk.stem.porter import PorterStemmer
 
-import logging
 
+# use logging to save the results
 logging.basicConfig(filename='naive_bayes_results.log', level=logging.INFO)
 
 stemmer = PorterStemmer()
@@ -32,10 +50,10 @@ def read_files(path):
     for label in ['negative', 'positive']:
         tmp = os.path.join(path, label)
         for file in os.listdir(tmp):
-            with open(os.path.join(tmp, file), 'r', encoding='utf-8') as f:
-                tweet = f.read().strip()
+            with open(os.path.join(tmp, file), 'r', encoding='utf-8') as f:   #join path to read all the tweets
+                tweet = f.read().strip()     # strip function to remove leading and trailing spaces in each tweet
                 corpus.append((tweet, 1 if label == 'positive' else 0))
-    df = pd.DataFrame.from_records(corpus, columns=['tweet', 'label'])
+    df = pd.DataFrame.from_records(corpus, columns=['tweet', 'label'])  #build dataframe
     return df
 
 
@@ -45,17 +63,17 @@ def tokenize(x, stem=False):
     for all capital words, handling of emoticons. we have created streams of tokens without stemming using word_tokenize
     as well as tokens with stemming using PotterStemmer.
     """
-    x = re.sub(r'(?:<[^>]+>)', '', x)
-    x = re.sub('([A-Z][a-z]+)', lambda t: t.group(0).lower(), x)
-    emoticon_tokens = re.split(emoticons_re, x)
+    x = re.sub(r'(?:<[^>]+>)', '', x)  #substitute html tags
+    x = re.sub('([A-Z][a-z]+)', lambda t: t.group(0).lower(), x)  # group 0 refers to A-Z, lowercase group 0
+    emoticon_tokens = re.split(emoticons_re, x)  # separate emoticons
     tokens = []
     for t in emoticon_tokens:
         if re.match(emoticons_re, t):
-            tokens.append(t)
+            tokens.append(t)   # append emoticons without work tokenize
         else:
             tokens += word_tokenize(x)
     if stem:
-        tokens = [stemmer.stem(t) for t in tokens]
+        tokens = [stemmer.stem(t) for t in tokens]  #perform stemming
     return tokens
 
 
@@ -66,7 +84,7 @@ def vocabulary(tokenized_tweets):
     vocab = set()
     for tokens in tokenized_tweets:
         vocab.update(tokens)
-    return list(vocab)
+    return list(vocab)   #create complete list of tokens for all the tweets
 
 
 def bag_of_words(tokenized_tweet, vocab, binary=True):
@@ -95,9 +113,10 @@ def preprocess(df, stem=False, binary=True, vocab=None):
         output = tokens.apply(lambda x: bag_of_words(x, vocab, False))
     return np.array(output.values.tolist()), vocab
 
+
 def train(features, labels):
     """
-    train - calculates the class prior and likelihoods for use in Naive Bayes predictions
+    train - calculates the class prior and likelihoods to be used in Naive Bayes predictions
     """
     if isinstance(labels, pd.Series):
         labels = labels.values
@@ -110,6 +129,7 @@ def train(features, labels):
         count_all = np.sum(count_wi)
         likelihood[c] = np.log((count_wi + 1) / (count_all + len(count_wi)))
     return dict(prior=prior, likelihood=likelihood, categories=categories)
+
 
 def predict(model, features):
     """
@@ -126,6 +146,7 @@ def predict(model, features):
     y_pred = np.array([model['categories'][x] for x in y_argmax])
     return y_pred
 
+
 def evaluate(y_true, y_pred, true_label=1):
     """
      evaluate - calculates and prints accuracy and confusion matrix for predictions
@@ -141,6 +162,7 @@ def evaluate(y_true, y_pred, true_label=1):
     logging.info('Accuracy = %2.2f' % (np.sum(y_true == y_pred) * 100 / len(y_pred)))
     logging.info('')
 
+
 def run(stem=False, binary=True):
     """
     run - Execution of appropriate functions as per the required call
@@ -154,6 +176,7 @@ def run(stem=False, binary=True):
     y_pred = predict(model, x_test)
     y_test = df_test.label.values
     evaluate(y_test, y_pred)
+
 
 def main():
     """

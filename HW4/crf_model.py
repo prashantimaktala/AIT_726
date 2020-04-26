417bae6716ad0fe079c1d0a42b10feee879bec8d
Yasas Senarath <ysenarath.93@gmail.com>
2020-04-25 21:51:05 -0400

    Add CRF Model.

--- HW4/crf_model.py
+++ HW4/crf_model.py
@@ -11,6 +11,62 @@ import sklearn_crfsuite
 from sklearn_crfsuite import scorers
 from sklearn_crfsuite import metrics
 
-columns = nltk.corpus.conll2002.fileids()
+from HW4.dataset import load_dataset
 
-print(columns)
+
+def word2features(sent, i):
+    word = sent[i][0]
+    postag = sent[i][1]
+    is_predicate = sent[i][2]
+    features = {
+        'bias': 1.0,
+        'word.lower()': word.lower(),
+        'word[-3:]': word[-3:],
+        'word[-2:]': word[-2:],
+        'word.isupper()': word.isupper(),
+        'word.istitle()': word.istitle(),
+        'word.isdigit()': word.isdigit(),
+        'postag': postag,
+        'postag[:2]': postag[:2],
+        'is_predicate': is_predicate,
+    }
+    return features
+
+
+def sent2features(sent):
+    return [word2features(sent, i) for i in range(len(sent))]
+
+
+def sent2labels(sent):
+    return [label for token, postag, is_predicate, _, label in sent]
+
+
+def sent2tokens(sent):
+    return [token for token, postag, is_predicate, _, label in sent]
+
+
+def main():
+    train_sents = load_dataset()
+    X_train = [sent2features(s) for s in train_sents]
+    y_train = [sent2labels(s) for s in train_sents]
+    crf = sklearn_crfsuite.CRF(
+        algorithm='lbfgs',
+        c1=0.1,
+        c2=0.1,
+        max_iterations=100,
+        all_possible_transitions=True
+    )
+    crf.fit(X_train, y_train)
+    test_sents = load_dataset('./data.wsj/test-set.txt')
+    X_test = [sent2features(s) for s in test_sents]
+    y_test = [sent2labels(s) for s in test_sents]
+    labels = list(crf.classes_)
+    labels.remove('O')
+    print('Labels: ' + ', '.join(labels))
+    y_pred = crf.predict(X_test)
+    f1_score = metrics.flat_f1_score(y_test, y_pred, average='weighted', labels=labels)
+    print('F1 Score: ' + str(f1_score))
+
+
+if __name__ == '__main__':
+    main()
--- HW4/dataset.py
+++ HW4/dataset.py
@@ -8,25 +8,58 @@ def preprocess(lst):
     return [x.strip() for x in lst]
 
 
-def load_dataset(path='./data.wsj/test-set.txt'):
-    NUM_TARGETS = 10
-    ds = []
-    sentence, targets = [], [[] for _ in range(NUM_TARGETS)]
+def boi_tags(tags):
+    result = []
+    last_tag = 'O'
+    for t in tags:
+        if t.startswith('(') and t.endswith(')'):
+            tag = 'B_' + t[1:-1].strip('*')
+            last_tag = 'O'
+        elif t.startswith('('):
+            tag = t[1:-1].strip('*')
+            last_tag = 'I_' + tag
+            tag = 'B_' + tag
+        elif t.endswith(')'):
+            tag = last_tag
+            last_tag = 'O'
+        else:
+            tag = last_tag
+        result.append(tag)
+    return result
+
+
+def load_dataset(path='./data.wsj/train-set.txt'):
+    N = 10
+    sentences = []
+    predicate_idx = -1
+    sentence, targets = [], [[] for _ in range(N)]
     for line in open(path, 'r', encoding='utf-8'):
         if len(line.strip()) == 0:
-            ds.append((sentence, targets))
-            sentence, targets = [], [[] for _ in range(NUM_TARGETS)]
+            for t in targets:
+                if len(t) != 0:
+                    sentences.append((sentence, t))
+            predicate_idx = -1
+            sentence, targets = [], [[] for _ in range(N)]
             continue
         line = re.split(" +", line.strip())
-        sentence.append(line[0])
+        sentence.append((line[0], line[1]))
+        if line[4] != '-':
+            predicate_idx += 1
         for idx, arg in enumerate(line[5:]):
-            try:
-                targets[idx].append((line[4], arg))
-            except:
-                print(idx)
-    return ds
+            pred = 0
+            if idx == predicate_idx and line[4] != '-':
+                pred = 1
+            targets[idx].append((pred, arg))
+    crf_format = []
+    for sent in sentences:
+        boi_targets = boi_tags([x[1] for x in sent[1]])
+        crf_format.append([
+            (token[0], token[1], target[0], target[1], boi_target) for token, target, boi_target in
+            zip(sent[0], sent[1], boi_targets)
+        ])
+    return crf_format
 
 
 if __name__ == '__main__':
     dataset = load_dataset()
-    print(dataset[1])
+    print(dataset[0])

95b26840577e326adcc364bdca1129216be7c821
Yasas Senarath <ysenarath.93@gmail.com>
2020-04-30 00:56:34 -0400

    Add pred see https://colab.research.google.com/drive/1vZzDD_LAt35skvwKc3rYY41BmSnY5wp0#scrollTo=C--xg5gTMwbc

--- HW4/crf_model.py
+++ HW4/crf_model.py
@@ -12,7 +12,7 @@ import sklearn_crfsuite
 from sklearn_crfsuite import scorers
 from sklearn_crfsuite import metrics
 
-from HW4.dataset import load_dataset
+from dataset import load_dataset, conll2003_tags
 
 plt.style.use('ggplot')
 
@@ -41,7 +41,7 @@ def sent2features(sent):
 
 
 def sent2labels(sent):
-    return [label for token, postag, is_predicate, _, label in sent]
+    return [s[-1] for s in sent]
 
 
 # def sent2tokens(sent):
@@ -103,6 +103,32 @@ def check_params(rs):
     print("Dark blue => {:0.4}, dark red => {:0.4}".format(min(_c), max(_c)))
 
 
+def predict_save(model, sentences):
+    lines_str = ''
+    for sentence, targets in sentences:
+        lines = [[w[2]] for w in sentence]
+        clens = [[0] for w in sentence]
+        for target in targets:
+            x_test = [sent2features([x + (t[0],) for x, t in zip(sentence, target)])]
+            # y_test = [sent2labels([t[1] for t in target])]
+            y_pred = model.predict(x_test)
+            y_pred = list(conll2003_tags(y_pred))[0]
+            for idx, (y_pred, clen) in enumerate(y_pred):
+                lines[idx] += [y_pred]
+                clens[idx] += [clen]
+        for i, line in enumerate(lines):
+            lines_str += line[0]
+            pre_len = 0txt
+            for j, p in enumerate(line[1:]):
+                lines_str += ' ' * (((24 - len(line[0])) if j == 0 else (16 - pre_len)) - cspace[i][j + 1])
+                lines_str += p
+                pre_len = len(p) - clens[i][j + 1]
+            lines_str += '\n'
+        lines_str += '\n'
+    with open('pred.txt', 'w', encoding='utf-8') as f:
+        f.write(lines_str)
+
+
 def main():
     train_sents = load_dataset()
     x_train = [sent2features(s) for s in train_sents]
@@ -115,26 +141,9 @@ def main():
         all_possible_transitions=True
     )
     crf.fit(x_train, y_train)
-    test_sents = load_dataset('./data.wsj/test-set.txt')
-    x_test = [sent2features(s) for s in test_sents]
-    y_test = [sent2labels(s) for s in test_sents]
-    labels = list(crf.classes_)
-    labels.remove('O')
-    print('Labels: ' + ', '.join(labels))
-    y_pred = crf.predict(x_test)
-    f1_score = metrics.flat_f1_score(y_test, y_pred, average='weighted', labels=labels)
-    print('F1 Score: ' + str(f1_score))
-    # group B and I results
-    sorted_labels = sorted(
-        labels,
-        key=lambda name: (name[1:], name[0])
-    )
-    print(metrics.flat_classification_report(
-        y_test, y_pred, labels=sorted_labels, digits=3
-    ))
-    rs = crf_cross_val_select(y_test, y_pred, labels)
-    check_params(rs)
-    predict_eval(rs, x_test, y_test, sorted_labels)
+    sentences = load_dataset('./data.wsj/test-set.txt', output=None)
+    predict_save(crf, sentences)
+    # $ perl srl-eval.pl data.wsj/props/test.wsj.props.test predictions.txt
 
 
 if __name__ == '__main__':
--- HW4/dataset.py
+++ HW4/dataset.py
@@ -28,21 +28,47 @@ def boi_tags(tags):
     return result
 
 
-def load_dataset(path='./data.wsj/train-set.txt'):
+def conll2003_tags(y_pred):
+    for s_pred in y_pred:
+        cdatas = []
+        for t_pred, t_pred_next in zip(s_pred, s_pred[1:] + [None]):
+            clen = 0
+            cdata = ''
+            if t_pred.startswith('I_'):
+                cdata = '*'
+            elif t_pred.startswith('B_'):
+                cdata = '(' + t_pred[2:] + '*'
+            else:
+                cdata = '*'
+            clen = len(cdata) - 1
+            if t_pred != 'O':
+                if t_pred_next is None or not t_pred_next.startswith('I_'):
+                    cdata += ')'
+            cdatas += [(cdata, clen)]
+        yield cdatas
+
+
+def load_dataset(path='./data.wsj/train-set.txt', output='crf'):
     N = 10
     sentences = []
     predicate_idx = -1
     sentence, targets = [], [[] for _ in range(N)]
     for line in open(path, 'r', encoding='utf-8'):
         if len(line.strip()) == 0:
-            for t in targets:
-                if len(t) != 0:
-                    sentences.append((sentence, t))
+            if output == 'crf':
+                for t in targets:
+                    if len(t) != 0:
+                        sentences.append((sentence, t))
+            else:
+                sentences.append((sentence, targets))
             predicate_idx = -1
             sentence, targets = [], [[] for _ in range(N)]
             continue
         line = re.split(" +", line.strip())
-        sentence.append((line[0], line[1]))
+        if output == 'crf':
+            sentence.append((line[0], line[1], line[4]))
+        else:
+            sentence.append((line[0], line[1]))
         if line[4] != '-':
             predicate_idx += 1
         for idx, arg in enumerate(line[5:]):
@@ -50,14 +76,16 @@ def load_dataset(path='./data.wsj/train-set.txt'):
             if idx == predicate_idx and line[4] != '-':
                 pred = 1
             targets[idx].append((pred, arg))
-    crf_format = []
-    for sent in sentences:
-        boi_targets = boi_tags([x[1] for x in sent[1]])
-        crf_format.append([
-            (token[0], token[1], target[0], target[1], boi_target) for token, target, boi_target in
-            zip(sent[0], sent[1], boi_targets)
-        ])
-    return crf_format
+    if output == 'crf':
+        crf_format = []
+        for sent in sentences:
+            boi_targets = boi_tags([x[1] for x in sent[1]])
+            crf_format.append([
+                (token[0], token[1], target[0], target[1], boi_target) for token, target, boi_target in
+                zip(sent[0], sent[1], boi_targets)
+            ])
+        return crf_format
+    return sentences
 
 
 if __name__ == '__main__':
